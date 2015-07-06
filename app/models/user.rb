a086e7ff7a6dc53ff5cbc1f5ed0e600d2243865a
Suresh Mahawar <suresh.mahawar1988@gmail.com>
2015-07-06 11:44:44 +0530

    attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation
+  #attr_accessible :email, :password, :password_confirmation
 
   attr_accessor :password
   before_save :encrypt_password
